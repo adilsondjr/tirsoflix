fc75c0b862228b9ed2921b3cd17a3448ea366737
Adilson Jr <adilsondjr@gmail.com>
2020-08-01 10:48:26 -0300

    Apagando tag div(conceito de react fragment) Adicionando cadastro de categoria Ajustando o css da pagina root

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact/>
       <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
       <Route component={() => (<div>Page Not Found 404!</div>)} />
     </Switch>
   </BrowserRouter>,
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom'
 
-function CadastroVideo() {
+function CadastroCategoria() {
     return (
         <PageDefault>
             <h1>Cadastro de video</h1>
 
-            <Link to="/cadastro/categoria">
-                Cadastro de Categoria
+            <Link to="/">
+                Home
             </Link>
         </PageDefault>
     )
 }
 
-export default CadastroVideo
+export default CadastroCategoria
